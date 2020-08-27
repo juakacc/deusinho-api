72fe94811413bdbba4c464741600dae95219e47c
Joaquim <juakacc@gmail.com>
2020-08-27 15:25:20 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./src/graphql/typedefs');
+const resolvers = require('./src/graphql/resolvers');
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  // eslint-disable-next-line no-console
+  console.log(`Server is running on ${url}`);
+});
