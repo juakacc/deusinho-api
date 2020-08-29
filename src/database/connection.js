db16108a295a541aef14b4cccdb64b2ad55974a0
Joaquim <juakacc@gmail.com>
2020-08-29 15:53:16 -0300

    Add cast of date from datetime on connection of knex

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,4 +1,5 @@
 const knex = require('knex');
+const moment = require('moment');
 
 const connection = knex({
   client: 'mysql',
@@ -7,6 +8,13 @@ const connection = knex({
     user: 'juakadb',
     password: 'juaka123',
     database: 'deusinho_api',
+    timezone: 'UTC',
+    typeCast(field, next) {
+      if (field.type === 'DATETIME') {
+        return moment(field.string()).format('YYYY-MM-DD HH:mm:ss');
+      }
+      return next();
+    },
   },
 });
 
